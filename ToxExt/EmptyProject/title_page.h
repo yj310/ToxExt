3527c596b3f0cb10f25c60a3bfd73ba9942f1325
lyj310 <yoenjii310@gmail.com>
2021-06-10 00:12:37 +0900

    fix title page

--- ToxExt/EmptyProject/title_page.h
+++ ToxExt/EmptyProject/title_page.h
@@ -9,16 +9,6 @@ public:
 	virtual ~TitlePage();
 
 	LPDIRECT3DTEXTURE9* backgroundTex;
-	LPDIRECT3DTEXTURE9* startButtonTex1;
-	LPDIRECT3DTEXTURE9* startButtonTex2;
-	LPDIRECT3DTEXTURE9* introButtonTex1;
-	LPDIRECT3DTEXTURE9* introButtonTex2;
-	LPDIRECT3DTEXTURE9* manualButtonTex1;
-	LPDIRECT3DTEXTURE9* manualButtonTex2;
-	LPDIRECT3DTEXTURE9* rankingButtonTex1;
-	LPDIRECT3DTEXTURE9* rankingButtonTex2;
-	LPDIRECT3DTEXTURE9* exitButtonTex1;
-	LPDIRECT3DTEXTURE9* exitButtonTex2;
 	LPD3DXSPRITE spr;
 	ID3DXFont* titleFont;
 	ID3DXFont* menuFont; 
